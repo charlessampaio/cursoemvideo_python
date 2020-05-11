f42392bc10cdd12eabcafcf324a193491bb72107
Charles Costa <64718047+charlessampaio@users.noreply.github.com>
2020-05-10 22:58:55 -0300

    Upload dos exercícios do 1 ao 5.

---
+++ ex005.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('(Resultado usando variável) Analisando o valor de {}, seu antecessor é {} e seu sucessor é {}!'.format (n, a, s))
+print('(Resultado sem variáveis) Analizando o valor de {}, seu antecessor é {} e seu sucessor é {}!'.format(n, (n-1),(n+1)))
